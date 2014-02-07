13c0cad10483b5deb3cf11fe4793c0f180b1412d
Yosihiro Isago <isago@praisefirst.jp>
2014-02-07 10:22:00 +0900

    テスト対象を初期化関数をNewHeapに変更

--- heap_test.go
+++ heap_test.go
@@ -18,33 +18,33 @@ func TestNewHeap(t *testing.T) {
 func TestHeap(t *testing.T) {
 	Describe(t, "Heap struct Tests", func() {
 		Context("push k:0 v:0", func() {
-			sut := make(Heap)
-			sut.Push(0, 0)
 			It("actual == expected", func() {
+				sut := NewHeap()
+				sut.Push(0, 0)
 				Expect(sut.Pop(0)).To(Equal, 0)
 			})
 		})
 		Context("push k:0 v:1", func() {
-			sut := make(Heap)
-			sut.Push(0, 1)
 			It("actual == expected", func() {
+				sut := NewHeap()
+				sut.Push(0, 1)
 				Expect(sut.Pop(0)).To(Equal, 1)
 			})
 		})
 		Context("push k:v 0:1 1:2", func() {
-			sut := make(Heap)
-			sut.Push(0, 1)
-			sut.Push(1, 2)
 			It("actual == expected", func() {
+				sut := NewHeap()
+				sut.Push(0, 1)
+				sut.Push(1, 2)
 				Expect(sut.Pop(0)).To(Equal, 1)
 				Expect(sut.Pop(1)).To(Equal, 2)
 			})
 		})
 		Context("push k:v 0:1 0:2", func() {
-			sut := make(Heap)
-			sut.Push(0, 1)
-			sut.Push(0, 2)
 			It("actual == expected", func() {
+				sut := NewHeap()
+				sut.Push(0, 1)
+				sut.Push(0, 2)
 				Expect(sut.Pop(0)).To(Equal, 2)
 			})
 		})
