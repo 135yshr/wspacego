e7072ef23cc0ca991193930779853461cf24926e
Yosihiro Isago <isago@praisefirst.jp>
2014-02-07 15:53:52 +0900

    コマンド作成関数とサブコマンド作成関数のテストケースを変更

--- command_test.go
+++ command_test.go
@@ -7,28 +7,26 @@ import (
 
 func TestCommnad(t *testing.T) {
 	Describe(t, "Command Tests", func() {
-		Context("Create Instance", func() {
-			It("Initialize Instance", func() {
+		Context("コマンドだけ指定する", func() {
+			It("インスタンスが作成できること", func() {
 				cmd := "cmd"
 				sut := NewCommand(cmd)
 				Expect(sut).To(Exist)
 			})
+			It("コマンドだけ変数が書き換えられていること", func() {
+				cmd := "cmd"
+				sut := NewCommand(cmd)
+				Expect(sut.cmd).To(Equal, cmd)
+				Expect(sut.subcmd).To(Equal, "")
+				Expect(sut.param).To(Equal, 0)
+			})
 		})
-		Context("Initial Instance", func() {
-			cmd := "cmd"
-			sut := NewCommand(cmd)
-			Expect(sut.cmd).To(Equal, cmd)
-			Expect(sut.subcmd).To(Equal, "")
-			Expect(sut.param).To(Equal, 0)
-		})
-		Context("Create Instance", func() {
-			It("is exists", func() {
+		Context("サブコマンドを作成する", func() {
+			It("インスタンスが作成できること", func() {
 				cmd, subcmd := "cmd", "subcmd"
 				Expect(NewSubCommand(cmd, subcmd)).To(Exist)
 			})
-		})
-		Context("Create Instance", func() {
-			It("Initialize Instance", func() {
+			It("コマンドとサブコマンドが指定した値で書き換えられていること", func() {
 				cmd, subcmd := "cmd", "subcmd"
 				sut := NewSubCommand(cmd, subcmd)
 				Expect(sut.cmd).To(Equal, cmd)
