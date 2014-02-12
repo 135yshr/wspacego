c04f94a3af40413b51aa23ca4e70caabe69a7ff4
Yosihiro Isago <isago@praisefirst.jp>
2014-02-12 16:08:23 +0900

    gotoを実行する命令を実装

--- interpreter_test.go
+++ interpreter_test.go
@@ -93,6 +93,15 @@ func TestInterpretor(t *testing.T) {
 				Expect(sut.commands.Len()).To(Equal, 1)
 				Expect(sut.commands.Get(1)).To(Equal, NewSubCommand("call", "1001"))
 			})
+			It("ラベルを呼び出すコマンドが作成されること", func() {
+				data = []byte{'G', 'o', 't', 'o', '\n', ' ', '\n', '\t', ' ', ' ', '\t', '\n'}
+				sut := NewInterpreter(data)
+				sut.filter()
+				sut.parseCommands()
+				Expect(sut.commands).To(Exist)
+				Expect(sut.commands.Len()).To(Equal, 1)
+				Expect(sut.commands.Get(1)).To(Equal, NewSubCommand("goto", "1001"))
+			})
 		})
 	})
 }
