e2185ba15bf943f61ae0ac9c6e2924debb810b77
Yosihiro Isago <isago@praisefirst.jp>
2014-02-13 11:51:47 +0900

    スタックトップと２番目の値を入れ替える命令が作成されることを確認するテストケースを追加

--- converter_test.go
+++ converter_test.go
@@ -49,6 +49,15 @@ func TestConerter(t *testing.T) {
 				Expect(cmd).To(Exist)
 				Expect(cmd).To(Equal, NewSubCommand("stack", "copy"))
 			})
+			It("スタックをコピーするコマンドが作成されること", func() {
+				data := []byte{'\n', '\t'}
+				sut := NewConverter()
+				cmd, seek, err := sut.stackManipulation(data)
+				Expect(err).To(NotExist)
+				Expect(seek).To(Equal, len(data))
+				Expect(cmd).To(Exist)
+				Expect(cmd).To(Equal, NewSubCommand("stack", "swap"))
+			})
 		})
 	})
 }
