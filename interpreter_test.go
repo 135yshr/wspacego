8bb59d083c0a102be0e327d6558ad9e0f7e8eae0
Yosihiro Isago <isago@praisefirst.jp>
2014-02-13 10:30:46 +0900

    解析できないパターンがきたときにエラーが発生することを確認

--- interpreter_test.go
+++ interpreter_test.go
@@ -261,6 +261,14 @@ func TestInterpretor(t *testing.T) {
 				Expect(sut.commands.Len()).To(Equal, 1)
 				Expect(sut.commands.Get(1)).To(Equal, NewCommand("getn"))
 			})
+			It("解析できないパターンができたときにエラーが作成されること", func() {
+				data = []byte{'u', 'n', 'k', 'o', 'w', 'n', '\t', '\n', '\t', '\n'}
+				sut := NewInterpreter(data)
+				sut.filter()
+				err := sut.parseCommands()
+				Expect(sut.commands).To(NotExist)
+				Expect(err).To(Exist)
+			})
 		})
 	})
 }
