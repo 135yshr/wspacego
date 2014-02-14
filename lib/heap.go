e7a84e24bdad63f369c8c2b01b9b2979bd016380
Yosihiro Isago <isago@praisefirst.jp>
2014-02-14 13:14:23 +0900

    heapのメソッド定義にポインタを付け加える

--- lib/heap.go
+++ lib/heap.go
@@ -2,15 +2,18 @@ package lib
 
 type Heap map[int]int
 
-func (h Heap) Push(k, v int) {
-	h[k] = v
-}
-
-func (h Heap) Pop(k int) int {
-	return h[k]
-}
-
 func NewHeap() *Heap {
 	ret := make(Heap)
 	return &ret
+}
+
+func (h *Heap) Push(k, v int) {
+	heap := *h
+	heap[k] = v
+	*h = heap
+}
+
+func (h *Heap) Pop(k int) int {
+	heap := *h
+	return heap[k]
 }
